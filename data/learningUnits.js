2a91baeba62ed408fd626daedc22990a3c4125d7
Izabela <muskala.iza@gmail.com>
2026-07-02 16:41:00 +0200

    Validate migrated learning units foundation

---
+++ data/learningUnits.js
@@ -0,0 +1,7 @@
+import { algorytmikaUnits } from "./learningUnits/algorytmika.js";
+import { programowanieUnits } from "./learningUnits/programowanie.js";
+
+export const learningUnits = [
+  ...algorytmikaUnits,
+  ...programowanieUnits,
+];

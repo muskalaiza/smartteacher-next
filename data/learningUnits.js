7af23c88456e6e66c1e5896428dab158203f1bad
Izabela <muskala.iza@gmail.com>
2026-07-27 23:33:17 +0200

    Remove legacy retrieval code

--- data/learningUnits.js
+++
@@ -1,7 +0,0 @@
-import { algorytmikaUnits } from "./learningUnits/algorytmika.js";
-import { programowanieUnits } from "./learningUnits/programowanie.js";
-
-export const learningUnits = [
-  ...algorytmikaUnits,
-  ...programowanieUnits,
-];
